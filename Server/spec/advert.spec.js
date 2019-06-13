d294ec38a136178037dcbc59b3a19efbcc7e64e3
kuzko58 <41537859+kuzko58@users.noreply.github.com>
2019-06-13 19:00:50 +0100

    server update 3

---
+++ Server/spec/advert.spec.js
@@ -0,0 +1,311 @@
+/* eslint-env jasmine */
+import Request from 'request';
+// import debug from 'debug';
+import Server from '../src/server';
+import storage from '../storage';
+
+const start = () => Server;
+start();
+
+
+describe('get specific advert', () => {
+  const data = {};
+  const options = {
+    url: 'http://localhost:3000/api/v1/advert/1',
+    headers: {
+      sender: 'user',
+    },
+  };
+  beforeAll((done) => {
+    Request.get(options, (error, response, body) => {
+      data.status = response.statusCode;
+      data.body = body;
+      done();
+    });
+  });
+  it('status 200', () => {
+    expect(data.status).toBe(200);
+  });
+  it('an advert object', () => {
+    expect(JSON.parse(data.body)).toEqual(storage.adverts.find(ad => ad.id === 1));
+  });
+});
+
+describe('get nonexistent advert', () => {
+  const data = {};
+  const options = {
+    url: 'http://localhost:3000/api/v1/adverts/003',
+    headers: {
+      sender: 'user',
+    },
+  };
+  beforeAll((done) => {
+    Request.get(options, (error, response, body) => {
+      data.status = response.statusCode;
+      data.body = body;
+      done();
+    });
+  });
+  it('status 404', () => {
+    expect(data.status).toBe(404);
+  });
+});
+
+describe('get all adverts', () => {
+  const data = {};
+  const options = {
+    url: 'http://localhost:3000/api/v1/advert/',
+    headers: {
+      sender: 'admin',
+    },
+  };
+  beforeAll((done) => {
+    Request.get(options, (error, response, body) => {
+      data.status = response.statusCode;
+      data.body = body;
+      done();
+    });
+  });
+  it('status 200', () => {
+    expect(data.status).toBe(200);
+  });
+  it('array of all adverts', () => {
+    expect(JSON.parse(data.body)).toEqual(storage.adverts);
+  });
+});
+
+describe('get all user adverts', () => {
+  const data = {};
+  const options = {
+    url: 'http://localhost:3000/api/v1/user/1/advert/',
+    headers: {
+      sender: 'user',
+    },
+  };
+  beforeAll((done) => {
+    Request.get(options, (error, response, body) => {
+      data.status = response.statusCode;
+      data.body = body;
+      done();
+    });
+  });
+  it('status 200', () => {
+    expect(data.status).toBe(200);
+  });
+  it('array of all user adverts', () => {
+    expect(JSON.parse(data.body).every(obj => obj.owner === 1)).toBe(true);
+  });
+});
+
+describe('get adverts by filter', () => {
+  const data = {};
+  const options = {
+    url: 'http://localhost:3000/api/v1/advert?status=available',
+    headers: {
+      sender: 'user',
+    },
+  };
+  beforeAll((done) => {
+    Request.get(options, (error, response, body) => {
+      data.status = response.statusCode;
+      data.body = body;
+      done();
+    });
+  });
+  it('status 200', () => {
+    expect(data.status).toBe(200);
+  });
+  it('array of all filtered adverts', () => {
+    expect(JSON.parse(data.body).every(obj => obj.status === 'available')).toBe(true);
+  });
+});
+
+describe('creating a new advert', () => {
+  const data = {};
+  const options = {
+    url: 'http://localhost:3000/api/v1/advert/',
+    json: true,
+    method: 'post',
+    headers: {
+      sender: 'user',
+    },
+    body: {
+      id: 5,
+      owner: 1,
+      created_on: '21-05-2019',
+      condition: 'New',
+      status: 'available',
+      price: 18000000,
+      manufacturer: 'Honda',
+      model: 'CR-V',
+      body_type: 'SUV',
+    },
+  };
+  beforeAll((done) => {
+    Request.post(options, (error, response) => {
+      data.status = response.statusCode;
+      done();
+    });
+  });
+  it('status 201', () => {
+    expect(data.status).toBe(201);
+  });
+  describe('check if advert was created', () => {
+    const data2 = {};
+    const options2 = {
+      url: 'http://localhost:3000/api/v1/advert/5',
+      headers: {
+        sender: 'user',
+      },
+    };
+    beforeAll((done) => {
+      Request.get(options2, (error, response, body) => {
+        data2.status = response.statusCode;
+        data2.body = body;
+        done();
+      });
+    });
+    it('status 200', () => {
+      expect(data2.status).toBe(200);
+    });
+    it('advert object', () => {
+      expect(JSON.parse(data2.body)).toEqual({
+        id: 5,
+        owner: 1,
+        created_on: '21-05-2019',
+        condition: 'New',
+        status: 'available',
+        price: 18000000,
+        manufacturer: 'Honda',
+        model: 'CR-V',
+        body_type: 'SUV',
+      });
+    });
+  });
+});
+
+describe('update advert status', () => {
+  const data = {};
+  const options = {
+    url: 'http://localhost:3000/api/v1/advert/1/status',
+    json: true,
+    method: 'patch',
+    headers: {
+      sender: 'user',
+    },
+    body: {
+      status: 'sold',
+    },
+  };
+  beforeAll((done) => {
+    Request.patch(options, (error, response) => {
+      data.status = response.statusCode;
+      done();
+    });
+  });
+  it('status 202', () => {
+    expect(data.status).toBe(202);
+  });
+  describe('check if advert status was updated', () => {
+    const data2 = {};
+    const options2 = {
+      url: 'http://localhost:3000/api/v1/advert/1',
+      headers: {
+        sender: 'user',
+      },
+    };
+    beforeAll((done) => {
+      Request.get(options2, (error, response, body) => {
+        data2.status = response.statusCode;
+        data2.body = body;
+        done();
+      });
+    });
+    it('status 200', () => {
+      expect(data2.status).toBe(200);
+      expect(JSON.parse(data2.body).status).toBe('sold');
+    });
+  });
+});
+
+describe('update advert price', () => {
+  const data = {};
+  const options = {
+    url: 'http://localhost:3000/api/v1/advert/1/price',
+    json: true,
+    method: 'patch',
+    headers: {
+      sender: 'user',
+    },
+    body: {
+      price: 14000000,
+    },
+  };
+  beforeAll((done) => {
+    Request.patch(options, (error, response) => {
+      data.status = response.statusCode;
+      done();
+    });
+  });
+  it('status 202', () => {
+    expect(data.status).toBe(202);
+  });
+  describe('check if advert price was updated', () => {
+    const data2 = {};
+    const options2 = {
+      url: 'http://localhost:3000/api/v1/advert/1',
+      headers: {
+        sender: 'user',
+      },
+    };
+    beforeAll((done) => {
+      Request.get(options2, (error, response, body) => {
+        data2.status = response.statusCode;
+        data2.body = body;
+        done();
+      });
+    });
+    it('status 200', () => {
+      expect(data2.status).toBe(200);
+      expect(JSON.parse(data2.body).price).toBe(14000000);
+    });
+  });
+});
+
+describe('deleting an advert', () => {
+  const data = {};
+  const options = {
+    url: 'http://localhost:3000/api/v1/advert/2',
+    headers: {
+      sender: 'admin',
+    },
+  };
+  beforeAll((done) => {
+    Request.delete(options, (error, response) => {
+      data.status = response.statusCode;
+      done();
+    });
+  });
+  it('status 200', () => {
+    expect(data.status).toBe(200);
+  });
+  describe('check if advert was deleted', () => {
+    const data2 = {};
+    const options2 = {
+      url: 'http://localhost:3000/api/v1/advert/2',
+      headers: {
+        sender: 'user',
+      },
+    };
+    beforeAll((done) => {
+      Request.get(options2, (error, response, body) => {
+        data2.status = response.statusCode;
+        data2.body = body;
+        done();
+      });
+    });
+    it('status 404', () => {
+      expect(data2.status).toBe(404);
+    });
+  });
+});
