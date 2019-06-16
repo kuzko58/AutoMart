185937a06f06de309b45fc75fd6813cedf16e044
kuzko58 <41537859+kuzko58@users.noreply.github.com>
2019-06-16 17:47:18 +0100

    UI mod

--- UI/scripts/sign in.js
+++ UI/scripts/sign in.js
@@ -1,33 +1,33 @@
 import {
-    main
+  main
 } from './main.js';
 
 function switchTab(x, y, a, b) {
-    y.style.color = 'lightgray';
-    x.style.color = 'mediumturquoise';
-    x.style.borderBottom = '1px solid mediumturquoise';
-    y.style.borderBottom = 'none';
-    document.querySelector(b).style.display = 'none';
-    document.querySelector(a).style.display = 'flex';
+  y.style.color = 'lightgray';
+  x.style.color = 'teal';
+  x.style.borderBottom = '1px solid lightgray';
+  y.style.borderBottom = 'none';
+  document.querySelector(b).style.display = 'none';
+  document.querySelector(a).style.display = 'flex';
 }
 
 window.addEventListener('load', () => {
-    let data = sessionStorage.page;
-    if (data === 'sign up') {
-        switchTab(signUp, signIn, '.sign-up', '.sign-in');
-    } else if (data === 'sign in') {
-        switchTab(signIn, signUp, '.sign-in', '.sign-up');
-    } else {
-        switchTab(signIn, signUp, '.sign-in', '.sign-up');
-    }
-    main();
+  let data = sessionStorage.page;
+  if (data === 'sign up') {
+    switchTab(signUp, signIn, '.sign-up', '.sign-in');
+  } else if (data === 'sign in') {
+    switchTab(signIn, signUp, '.sign-in', '.sign-up');
+  } else {
+    switchTab(signIn, signUp, '.sign-in', '.sign-up');
+  }
+  main();
 });
 
 
 signUp.addEventListener('click', () => {
-    switchTab(signUp, signIn, '.sign-up', '.sign-in');
+  switchTab(signUp, signIn, '.sign-up', '.sign-in');
 });
 
 signIn.addEventListener('click', () => {
-    switchTab(signIn, signUp, '.sign-in', '.sign-up');
+  switchTab(signIn, signUp, '.sign-in', '.sign-up');
 });
